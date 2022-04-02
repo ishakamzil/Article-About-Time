b4c78139176c2c2d6bb0114d29efbf87f4260111
ishakamzil <91615454+ishakamzil@users.noreply.github.com>
2022-04-02 18:46:11 +0000

    upload

---
+++ script.js
@@ -0,0 +1,303 @@
+let arabic = document.getElementById("arabic");
+let русский = document.getElementById("русский");
+let Français = document.getElementById("Français");
+let 日本 = document.getElementById("日本");
+
+let pagtitle = document.getElementById("pagtitle");
+let title = document.getElementById("title");
+
+let part1 = document.getElementById("part1");
+let pre1 = document.getElementById("pre1");
+
+let part2 = document.getElementById("part2");
+let pre2 = document.getElementById("pre2");
+
+let part3 = document.getElementById("part3");
+let pre3 = document.getElementById("pre3");
+
+
+arabic.onclick = function () {
+   pagtitle.innerHTML = `أهمية الوقت`;
+
+   title.innerHTML = `مقال عن أهمية الوقت`;
+
+   part1.innerHTML = `أهمية الوقت`;
+    pre1.innerHTML = `الوقت أهم وأثمن من المال ، رغم أن معظم الناس سيارات
+   المزيد عن المال أكثر من الوقت ؛ حيث لا يمكن استخدام المال لشراء المزيد والمزيد من الوقت ،
+  ولكن في المقابل يمكن استخدام الوقت لكسب الكثير من المال. [1]
+
+  كل شخص في الكون له يوم واحد فقط أربع وعشرون ساعة ، وفيه كل دقيقة
+   مهم ويجب استخدامه ؛ لأنه لا يمكن أن يعود بعد ذهابه ، الشخص الذي لديه
+   حقق الكثير هو الشخص الذي قضى وقتًا طويلاً في العمل الشاق ؛
+   لتحقيق وإتقان المهارات المطلوبة. [1]`;
+
+   part2.innerHTML = `فوائد إدارة الوقت`;
+    pre2.innerHTML = `
+   هناك عدد من الفوائد التي يمكن الحصول عليها إذا تمت إدارة الوقت بشكل أفضل ، وهي كذلك
+    هي كما يلي: [2] تحقيق الكثير من الإنجازات في وقت أقصر وجهد أقل ،
+   من خلال تحديد أولويات المهام ومحاولة استغلال الوقت لتنفيذها.
+  
+   الشعور بجمال العمل وأن الحياة لها هدف ؛ لأن قضاء الوقت بدون
+   الإنتاج هو أحد أكثر الأشياء المزعجة والمحبطة للفرد ،
+   يساعد الشعور بالإنجاز على مواصلة العمل ، وبالتالي
+    القدرة على الاسترخاء والاستمتاع بالوقت.
+  
+   تخلصوا من التردد والارتباك عند القلق من القرارات المتخذة سواء كانت كذلك
+   صحيح أم لا ، هذا مضيعة للوقت. تحقيق الأهداف بسرعة عندما لا تقدر و
+    فهم أهمية إدارة الوقت ، فقد تظل هذه الأهداف على الجانب إلى أجل غير مسمى.
+  
+  تعزيز الثقة بالنفس ، من خلال الاعتناء بنفسك بشكل صحيح وتخصيص الوقت لها.
+  
+  الحصول على الكثير من الطاقة ، والقيام بالكثير من العمل ، وبناء مهارات جديدة ؛ لتحسين العمل ،
+   عندما تشعر بوجود نشاط وزيادة في الطاقة ،
+  
+  يمكنك تجربة مغامرة جميلة ، أو تجربة هواية بدلاً من البقاء على الأريكة.
+  
+  فعل الأشياء التي يحبها الإنسان بعد تحديد الوقت اللازم للنوم والعمل والأكل.
+   وغيرهم ، هناك أربع ساعات في اليوم لتكريسها للمتعة.`;
+
+
+   part3.innerHTML = `طرق استخدام الوقت`;
+    pre3.innerHTML = `هناك عدد من الأشياء التي يمكن أن تتجنب تضييع الوقت والاستفادة منه ،
+    وهي كالتالي: [3]
+
+   قم بإيقاف تشغيل الهاتف قدر الإمكان: يستغرق الأمر وقتًا طويلاً ، لذلك يمكن استخدامه أكثر
+    بشكل منتج ، ويجب إيقاف تشغيل الهاتف عند القيام بعمل آخر ، أو يمكن وضعه
+    في مكان بعيد المنال إذا كان من الضروري تشغيله ، بالإضافة إلى ذلك
+   إلى إمكانية إيقاف تشغيل أي إشعارات على الهاتف غير مهم للعمل.
+  
+   أغلق المتصفحات غير الضرورية: يعتمد الكثير من الأشخاص على الكمبيوتر والإنترنت للحصول عليها
+   انتهى العمل. العمل أثناء وجود Facebook أو Twitter أو غيرهم على المتصفح في علامات تبويب أخرى
+   قد تؤثر على مهارات إدارة الوقت.
+  
+  حظر وحظر بعض الوسائط الاجتماعية: في بعض الأحيان إغراء تسجيل الدخول إلى الشبكات الاجتماعية
+   وسائل الإعلام أمر لا مفر منه.
+  
+  تجنب الانقطاعات المتكررة قدر الإمكان: التوقف عن منتصف المهمة التي يجب القيام بها
+  شيء آخر قد يجعل من الصعب العودة إلى العمل السابق ؛
+  لذلك عند القيام بمهمة ما ، يجب أن تحاول إكمالها قبل البدء في شيء ما.`;
+
+
+
+  
+ 
+   document.body.style.direction = "rtl"
+   pre1.style.direction = "rtl"
+   pre2.style.direction = "rtl"
+   pre3.style.direction = "rtl"
+}
+
+русский.onclick = function () {
+   pagtitle.innerHTML = `важность времени`;
+
+   title.innerHTML = `Эссе о важности времени`;
+
+   part1.innerHTML = `важность времени`;
+    pre1.innerHTML = ` Время важнее денег, хотя большинство людей — машины.
+    Больше о деньгах, чем о времени; Где деньги не могут быть использованы,
+     чтобы купить все больше и больше времени,Но взамен время можно использовать, 
+     чтобы заработать много денег. [1]
+
+   У каждого человека во Вселенной есть только один день, двадцать четыре часа, 
+   и в нем каждая минута важно и должно быть использовано; Потому что он не может 
+   вернуться после того, как ушел, тот, кто Человек, потративший много времени на 
+   кропотливую работу, многого добился; Для достижения и овладения необходимыми навыками. [1]`;
+
+   part2.innerHTML = `Преимущества тайм-менеджмента`;
+    pre2.innerHTML = `Есть ряд преимуществ, которые можно получить, если лучше управлять временем, 
+    и они Они заключаются в следующем: [2] Достижение большего количества достижений за более короткое 
+    время и с меньшими усилиями, Расставляя приоритеты задач и стараясь использовать время для 
+    их выполнения.
+  
+   ощущение красоты работы и смысла жизни; Потому что проводить время без
+   Производство — одна из самых раздражающих и разочаровывающих вещей для человека.
+   Чувство выполненного долга помогает продолжать работу, поэтому
+    Возможность расслабиться и насладиться временем.
+  
+   Избавьтесь от колебаний и растерянности, когда беспокоитесь о принятых решениях, будь то
+   Правда это или нет, это пустая трата времени. Быстрое достижение целей, когда вы не можете и
+    Поймите важность тайм-менеджмента, эти цели могут оставаться в стороне на неопределенный срок.
+  
+  Повышение уверенности в себе, заботясь о себе должным образом и уделяя этому время.
+  
+  Иметь много энергии, много работать, приобретать новые навыки; улучшить работу,
+   Когда вы почувствуете присутствие активности и прилив энергии,
+  
+  Вместо того, чтобы лежать на диване, вы можете отправиться в приятное приключение или заняться хобби.
+  
+  Заниматься любимым делом после определения времени, необходимого для сна, работы и приема пищи.
+   А у других есть четыре часа в день, которые можно посвятить удовольствию.`;
+
+
+   part3.innerHTML = `  Способы использования времени`;
+    pre3.innerHTML = `Есть ряд вещей, на которые можно не тратить время и извлечь из этого пользу,
+    Они следующие: [3]
+
+   Выключите телефон как можно чаще: это занимает много времени, поэтому его можно использовать больше
+    продуктивно, а телефон надо выключать при выполнении другой работы, или его можно поставить
+    В недоступном месте при необходимости включения, дополнительно
+   Чтобы возможность отключения любых уведомлений на телефоне для работы не важна.
+  
+   Закройте ненужные браузеры: многие люди зависят от компьютера и Интернета, чтобы получить их.
+   работа сделана. Работайте, пока Facebook, Twitter или другие приложения находятся в браузере на
+    других вкладках. Может повлиять на навыки тайм-менеджмента.
+  
+  Блокировать и блокировать некоторые социальные сети: иногда возникает соблазн войти в социальные
+   сети СМИ неизбежны.
+  
+  По возможности избегайте частых прерываний: останавливайтесь посреди задачи, которую 
+  необходимо выполнить. Еще один момент, который может затруднить возвращение к прежней работе;
+  Поэтому, выполняя задачу, вы должны попытаться завершить ее, прежде чем что-то начинать.`;
+
+  document.body.style.direction = "ltr"
+   pre1.style.direction = "ltr"
+   pre2.style.direction = "ltr"
+   pre3.style.direction = "ltr"
+
+}
+
+日本.onclick = function () {
+   pagtitle.innerHTML = `時間の重要性`;
+
+   title.innerHTML = ` 時間の重要性に関するエッセイ`;
+
+   part1.innerHTML = `時間の重要性`;
+    pre1.innerHTML = `ほとんどの人は車ですが、時間はお金よりも重要です
+    時間よりもお金について。お金を使って時間を増やすことができない場合は、
+   しかし、見返りに時間を使ってたくさんのお金を稼ぐことができます。 [1]
+ 
+   宇宙のすべての人はたった1日24時間しかなく、その中には毎分
+    重要であり、使用する必要があります。彼が去った後、彼は戻ることができないので、
+    懸命に仕事に多くの時間を費やした人は、多くのことを成し遂げました。
+    必要なスキルを達成し、習得する。 [1] `;
+
+   part2.innerHTML = ` 時間管理のメリット`;
+    pre2.innerHTML = `
+    時間をうまく管理すれば得られるメリットはたくさんありますが、
+    それらは次のとおりです。[2]より短い時間とより少ない労力で多くの成果を達成し、
+   タスクに優先順位を付け、時間を使ってタスクを実装しようとする。
+  
+   仕事の美しさを感じ、その人生には目的があります。せずに時間を過ごすので
+   制作は、個人にとって最も迷惑で苛立たしいことの1つです。
+   達成感は仕事を続けるのに役立ちますので
+    リラックスして時間を楽しむ能力。
+    決定が下されたかどうかを心配するときは、ためらいや混乱を取り除きます。
+   本当かどうかは別として、これは時間の無駄です。できないときに目標をすばやく達成する
+    時間管理の重要性を理解してください、これらの目標は無期限に側にとどまるかもしれません。
+  
+  適切に自分の世話をし、そのための時間を作ることによって、自信を高めます。
+  
+  たくさんのエネルギーを持ち、たくさんの仕事をし、新しいスキルを身につけましょう。仕事を改善するために、
+   活動の存在とエネルギーの急増を感じるとき、
+  
+  ソファにとどまる代わりに、素敵な冒険を試したり、趣味を試したりすることができます。
+  
+  睡眠、仕事、食事に必要な時間を決めた後、人が好きなことをする。
+   そして他の人は、喜びに専念するために1日4時間あります。`;
+
+
+   part3.innerHTML = `時間を使う方法`;
+    pre3.innerHTML = `時間を無駄にすることを避け、それから利益を得ることができる多くのことがあります、
+    それらは次のとおりです。[3]
+
+   できるだけ電話の電源を切ってください：時間がかかるので、もっと使うことができます
+    生産的に、そして他の仕事をするとき電話はオフにされなければなりません、さもなければそれは置かれることができます
+    さらに、電源を入れる必要がある場合は手の届かない場所で
+   電話での通知をオフにする可能性は、仕事にとって重要ではありません。
+
+   不要なブラウザを閉じる：多くの人は、ブラウザを取得するためにコンピュータとインターネットに依存しています
+   作業は完了です。 Facebook、Twitterなどがブラウザのその他のタブにあるときに作業する
+   時間管理スキルに影響を与える可能性があります。
+  
+  一部のソーシャルメディアをブロックしてブロックする：ソーシャルネットワークにログインしたいという誘惑が時々あります
+   メディアは避けられない。
+  
+  頻繁な中断はできるだけ避けてください。実行する必要のあるタスクの途中で停止してください。
+  前の仕事に戻るのを難しくするかもしれないもう一つのこと。
+  したがって、タスクを実行するときは、何かを開始する前にタスクを完了するようにしてください。`;
+
+
+
+  
+ 
+   document.body.style.direction = "ltr"
+   pre1.style.direction = "ltr"
+   pre2.style.direction = "ltr"
+   pre3.style.direction = "ltr"
+}
+
+Français.onclick = function () {
+   pagtitle.innerHTML = `signification temporelle` ;
+
+title.innerHTML = `Article sur l'importance du temps` ;
+
+
+part1.innerHTML = `Importance temporelle` ;
+
+ pre1.innerHTML = `Le temps est plus important que l'argent, même si la plupart des gens sont des voitures
+Plus d'argent que de temps; Où l'argent ne peut pas être utilisé pour acheter de plus en plus de temps,
+Mais en retour, le temps peut être utilisé pour gagner beaucoup d'argent. [1]
+
+Chaque personne dans l'univers n'a qu'un jour, vingt-quatre heures, et chaque minute
+important et doit être utilisé ; Parce qu'il ne peut pas revenir après son départ, celui qui a
+Une personne qui a passé beaucoup de temps à travailler dur a beaucoup accompli ;
+Acquérir et maîtriser les compétences requises. [1]` ;
+
+
+part2.innerHTML = `Avantages de la gestion du temps` ;
+
+ pre2.innerHTML = `
+Il y a un certain nombre d'avantages qui peuvent être obtenus si le temps est mieux géré, et ils sont
+ Ils sont les suivants : [2] Atteindre plus de réalisations en moins de temps et avec moins d'efforts,
+En priorisant les tâches et en essayant d'utiliser le temps pour les mettre en œuvre.
+
+sentir la beauté du travail et que la vie a un but ; Parce que passer du temps sans
+La production est l'une des choses les plus ennuyeuses et frustrantes pour un individu,
+Un sentiment d'accomplissement aide à poursuivre le travail, donc
+ La capacité de se détendre et de profiter du temps.
+
+Débarrassez-vous de l'hésitation et de la confusion lorsque vous vous inquiétez des décisions prises, 
+qu'elles soient Vrai ou pas, c'est une perte de temps. Atteindre des objectifs rapidement quand vous
+ ne pouvez pas etComprenez l'importance de la gestion du temps, ces objectifs peuvent rester de 
+ côté indéfiniment.
+
+Renforcer la confiance en soi, en prenant bien soin de soi et en prenant du temps pour cela.
+
+Avoir beaucoup d'énergie, faire beaucoup de travail, acquérir de nouvelles compétences ; améliorer le 
+travail,Lorsque vous sentez la présence d'activité et une poussée d'énergie,
+
+Vous pouvez tenter une belle aventure ou essayer un passe-temps au lieu de rester sur le canapé.
+
+Faire les choses qu'une personne aime après avoir déterminé le temps nécessaire pour dormir,
+ travailler et manger.et d'autres, il y a quatre heures dans la journée à consacrer au plaisir.`
+
+
+
+part3.innerHTML = "`méthodes d'utilisation du temps`";
+
+ pre3.innerHTML = `Il y a un certain nombre de choses qui peuvent éviter de perdre du temps et en 
+ tirer profit, Ils sont les suivants : [3]
+
+Éteignez le téléphone autant que possible : cela prend beaucoup de temps, il peut donc être utilisé plus
+ de manière productive, et le téléphone doit être éteint lorsque vous effectuez d'autres travaux,
+  ou il peut être placé Dans un endroit hors de portée s'il est nécessaire de l'allumer, en plus
+La possibilité de désactiver les notifications sur le téléphone n'est pas importante pour le travail.
+
+Fermez les navigateurs inutiles : de nombreuses personnes dépendent de l'ordinateur et d'Internet pour
+ les obtenir.le travail est fait. Travaillez pendant que Facebook, Twitter ou d'autres sont sur
+  le navigateur dans d'autres ongletsPeut affecter les compétences en gestion du temps.
+
+Bloquer et bloquer certains réseaux sociaux : parfois la tentation de se connecter aux réseaux sociaux
+Les médias sont inévitables.
+
+Évitez autant que possible les interruptions fréquentes : arrêtez-vous au milieu d'une tâche qui doit
+ être faite Une autre chose qui peut rendre difficile le retour au travail précédent ;
+Ainsi, lorsque vous effectuez une tâche, vous devez essayer de la terminer avant de commencer 
+quelque chose.`;
+  
+ 
+   document.body.style.direction = "ltr"
+   pre1.style.direction = "ltr"
+   pre2.style.direction = "ltr"
+   pre3.style.direction = "ltr"
+}
